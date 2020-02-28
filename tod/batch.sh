91c1b8ba1ed2c707b1c04095855cdf2bb893b1fd
Ranajoy Banerji <ranajoyb@astro.uio.no>
2020-02-28 12:13:07 +0100

    some more changes

---
+++ tod/batch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+CONFIG_FILE=test.yaml
+VERBOSITY=2
+NUM_PROC=1
+OUT_FILE=out.txt
+
+mpirun -n $NUM_PROC python sim_timestream.py $CONFIG_FILE mpi -v $VERBOSITY
+# mpirun -n $NUM_PROC python sim_timestream.py $CONFIG_FILE mpi -v $VERBOSITY > $OUT_FILE 2>&1
