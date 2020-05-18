2999ba06cf673f5618af9bdbe17d33b923782b72
Ranajoy Banerji <ranajoyb@astro.uio.no>
2020-05-18 13:21:41 +0200

    full working code

--- tod/batch.sh
+++ tod/batch.sh
@@ -5,7 +5,8 @@ VERBOSITY=2
 NUM_PROC=48
 OUT_FILE=out.txt
 
-mpirun -n $NUM_PROC python sim_tod.py $CONFIG_FILE mpi -v $VERBOSITY
+# mpirun -n $NUM_PROC python sim_tod.py $CONFIG_FILE mpi -v $VERBOSITY
+mpirun -n $NUM_PROC python huff_compress.py $CONFIG_FILE
 # python sim_tod.py $CONFIG_FILE serial -v $VERBOSITY
 # mpirun -n $NUM_PROC python sim_tod.py $CONFIG_FILE mpi -v $VERBOSITY > $OUT_FILE 2>&1
 # mprof run python sim_tod.py $CONFIG_FILE serial -v $VERBOSITY
